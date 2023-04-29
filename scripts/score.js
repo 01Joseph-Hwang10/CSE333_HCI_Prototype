e73fc7a77cdab0525df2816f650c29bf044fe58a
01Joseph-Hwang10 <joseph95501@gmail.com>
2023-04-29 19:01:38 +0900

    add scroll behavior

--- scripts/score.js
+++ scripts/score.js
@@ -1,32 +1,49 @@
-const removeWebgazerVideoContainer = async () => {
-  return new Promise((resolve) => {
-    const interval = setInterval(() => {
-      const videoFeedback = document.querySelector("#webgazerVideoContainer");
-      if (videoFeedback) {
-        videoFeedback.style.display = "none";
-        clearInterval(interval);
-        resolve();
+const attachGazeListener = async () => {
+  await webgazer
+    .showVideoPreview(false)
+    .showPredictionPoints(false)
+    .setGazeListener(function (data, elapsedTime) {
+      console.log(data);
+      if (data == null) {
+        return;
       }
-    }, 100);
+      let xprediction = data.x;
+      let yprediction = data.y;
+      updateCursor(xprediction, yprediction);
+    })
+    .saveDataAcrossSessions(true)
+    .begin();
+};
+
+const attachScrollListener = () => {
+  const top = document.querySelector("#top");
+  // Scroll to up when the mouse cursor is inside the top div
+  top.addEventListener("mouseenter", () => {
+    window.scrollBy({
+      top: -500,
+      behavior: "smooth",
+    });
+  });
+
+  const bottom = document.querySelector("#bottom");
+  // Scroll to down when the mouse cursor is inside the bottom div
+  bottom.addEventListener("mouseenter", () => {
+    window.scrollBy({
+      top: 500,
+      behavior: "smooth",
+    });
   });
 };
 
 const initialize = async () => {
-  // webgazer
-  //   .setGazeListener(function (data, elapsedTime) {
-  //     console.log(data);
-  //     if (data == null) {
-  //       return;
-  //     }
-  //     let xprediction = data.x;
-  //     let yprediction = data.y;
-  //     updateCursor(xprediction, yprediction);
-  //   })
-  //   .begin();
-  // await removeWebgazerVideoContainer();
+  // await attachGazeListener();
   attachMousemoveListener();
+  attachScrollListener();
   populateDots();
   animate();
+  window.onbeforeunload = function () {
+    webgazer.end();
+  };
 };
 
 const everythingLoaded = setInterval(() => {
